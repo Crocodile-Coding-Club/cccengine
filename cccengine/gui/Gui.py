76926e3bc32af8f4a8d3a31939d79a5bb1257fb7
dalmatheo <134540422+dalmatheo@users.noreply.github.com>
2025-02-06 19:27:13 +0100

    Quelques petites modifs pour des tests

--- cccengine/gui/Gui.py
+++ cccengine/gui/Gui.py
@@ -1,4 +1,6 @@
 import pygame
+import Button
+
 
 class BackgroundImage(pygame.sprite.Sprite):
     def __init__(self, image, x, y, width, height, position: str = "topleft"):
@@ -18,9 +20,21 @@ class BackgroundImage(pygame.sprite.Sprite):
 
 
 class Gui:
-    def __init__(self, screen, backgroundImage, x, y, width, height, buttons: list = None, position: str = "topleft"):
-        self.screen = screen
-        self.backgroundImage = BackgroundImage(backgroundImage, x, y, width, height, position)
+    def __init__(
+        self,
+        screen: pygame.surface.Surface,
+        backgroundImage: pygame.surface.Surface,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        buttons: list[Button.Button] = None,
+        position: str = "topleft",
+    ):
+        self.screen: pygame.surface.Surface = screen
+        self.backgroundImage = BackgroundImage(
+            backgroundImage, x, y, width, height, position
+        )
         self.surface = self.backgroundImage.image
         self.buttons = pygame.sprite.Group()
         if buttons != None:
@@ -28,8 +42,8 @@ class Gui:
                 self.buttons.add(button)
 
     def update(self):
-       self.buttons.update()
-    
+        self.buttons.update()
+
     def draw(self):
         self.screen.blit(self.backgroundImage.image, self.backgroundImage.rect)
-        #self.buttons.draw(surface=self.surface)
+        # self.buttons.draw(surface=self.surface)
